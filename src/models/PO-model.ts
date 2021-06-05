1f0337d3b2b078fea900b19a24300a55cf5c9e0f
rodericusifo <rodericus1999@gmail.com>
2021-06-05 21:33:20 +0700

    PO Create complete

---
+++ src/models/PO-model.ts
@@ -0,0 +1,42 @@
+import { Schema, Model, model } from 'mongoose';
+import { IPODoc } from '../interfaces/PO-doc-interface';
+
+const POSchema = new Schema(
+    {
+        PONumber: { type: String, trim: true, unique: true, required: true },
+        PODate: { type: Date, required: true },
+        shipTo: {
+            name: { type: String, trim: true, required: true },
+            companyName: { type: String, trim: true, required: true },
+            phoneNumber: { type: String, trim: true, required: true },
+            email: { type: String, trim: true, required: true }
+        },
+        INVNumber: {
+            type: String,
+            trim: true,
+            unique: true,
+            default: undefined
+        },
+        INVDate: { type: Date, default: undefined },
+        billTo: {
+            name: { type: String, trim: true, default: undefined },
+            companyName: { type: String, trim: true, default: undefined },
+            phoneNumber: { type: String, trim: true, default: undefined },
+            email: { type: String, trim: true, default: undefined }
+        },
+        status: { type: String, trim: true, default: undefined },
+        subTotal: { type: Number, min: 0, default: 0 },
+        total: { type: Number, min: 0, default: 0 },
+        Supplier: {
+            type: Schema.Types.ObjectId,
+            ref: 'Supplier',
+            required: true
+        },
+        INVCreated: { type: Boolean, default: false }
+    },
+    { timestamps: true }
+);
+
+const POModel: Model<IPODoc> = model<IPODoc>('PO', POSchema);
+
+export { POModel };
---
+++ src/routers/PO-router.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import { POController } from '../controllers/PO-controller';
+import { JWTAuthorization } from '../middlewares/JWT-authorization-middleware';
+
+class RPO {
+    private router: Router;
+
+    constructor(router: Router) {
+        this.router = router;
+        this.routers();
+    }
+
+    public getRouter(): Router {
+        return this.router;
+    }
+
+    private routers(): void {
+        this.router.use(JWTAuthorization.verifyAccessToken);
+        this.router.post(
+            '/create',
+            [JWTAuthorization.ownerInventoryAuthorization],
+            POController.create
+        );
+    }
+}
+
+const PORouter = new RPO(Router()).getRouter();
+
+export { PORouter };
